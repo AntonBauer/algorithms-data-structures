062775b0914284b157010ff90623e992816a3792
Anton Bauer <abton.bauer@gmail.com>
2024-02-14 22:05:43 +0100

    [Data structures, Binary Search Tree] implement add with loop instead of recursion

--- src/DataStructures.Trees/BinarySearchTrees/BinarySearchTree.cs
+++ src/DataStructures.Trees/BinarySearchTrees/BinarySearchTree.cs
@@ -6,32 +6,57 @@ public class BinarySearchTree<TData> : ICollection<TData>
 {
   private readonly Comparer<TData> _comparer;
   private Node<TData>? _root;
-  
+
   public int Count { get; private set; }
-  
+
   public bool IsReadOnly => false;
 
   public BinarySearchTree() : this(Comparer<TData>.Default)
   {
   }
-  
+
   public BinarySearchTree(IComparer<TData> comparer)
   {
     var dataType = typeof(TData);
     // ToDo: create comparer delegate
     // dataType.GetMethod(nameof(IComparable<TData>.CompareTo)).CreateDelegate()
-    
+
     _comparer = dataType.IsAssignableTo(typeof(IComparable<TData>))
-      ? Comparer<TData>.Default 
+      ? Comparer<TData>.Default
       : Comparer<TData>.Create(comparer.Compare);
   }
-  
+
   public void Add(TData item)
   {
     if (Count == 0)
       _root = new Node<TData>(item, null, null, null);
 
-    _root!.Add(item, _comparer);
+
+    var current = _root;
+    while (true)
+    {
+      if (_comparer.Compare(current!.Data, item) > 0)
+      {
+        if (current.Left is null)
+        {
+          current.Left = new Node<TData>(item, current, default, default);
+          break;
+        }
+
+        current = current.Left;
+      }
+      else
+      {
+        if (current.Right is null)
+        {
+          current.Right = new Node<TData>(item, current, default, default);
+          break;
+        }
+
+        current = current.Right;
+      }
+    }
+
     ++Count;
   }
 
--- src/DataStructures.Trees/BinarySearchTrees/Node.cs
+++ src/DataStructures.Trees/BinarySearchTrees/Node.cs
@@ -5,24 +5,7 @@ internal record Node<TData>(TData Data, Node<TData>? Parent, Node<TData>? Left, 
   public readonly TData Data = Data;
   
   public Node<TData>? Parent { get; private set; } = Parent;
-  public Node<TData>? Left { get; private set; } = Left;
-  public Node<TData>? Right { get; private set; } = Right;
+  public Node<TData>? Left { get; set; } = Left;
+  public Node<TData>? Right { get; set; } = Right;
 
-  public void Add(TData data, Comparer<TData> comparer)
-  {
-    if(comparer.Compare(Data, data) > 0)
-    {
-      if (Left is null)
-        Left = new Node<TData>(data, this, default, default);
-      else
-        Left.Add(data, comparer);
-    }
-    else
-    {
-      if (Right is null)
-        Right = new Node<TData>(data, this, default, default);
-      else
-        Right.Add(data, comparer);
-    }
-  }
 }
