8c33b6574044dc04d08e75c5244eabe4ac5c274a
Anton Bauer <abton.bauer@gmail.com>
2024-02-14 21:57:20 +0100

    [Data structures, Binary Search Tree] implement add items

---
+++ src/DataStructures.Trees/BinarySearchTrees/BinarySearchTree.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace DataStructures.Trees.BinarySearchTrees;
+
+public class BinarySearchTree<TData> : ICollection<TData>
+{
+  private Comparer<TData> _comparer;
+  private Node<TData>? _root;
+  
+  public int Count { get; private set; }
+  
+  public bool IsReadOnly => false;
+
+  public BinarySearchTree() : this(Comparer<TData>.Default)
+  {
+  }
+  
+  public BinarySearchTree(IComparer<TData> comparer)
+  {
+    var dataType = typeof(TData);
+    // ToDo: create comparer delegate
+    // dataType.GetMethod(nameof(IComparable<TData>.CompareTo)).CreateDelegate()
+    
+    _comparer = dataType.IsAssignableTo(typeof(IComparable<TData>))
+      ? Comparer<TData>.Default 
+      : Comparer<TData>.Create(comparer.Compare);
+  }
+  
+  public void Add(TData item)
+  {
+    if (Count == 0)
+      _root = new Node<TData>(item, null, null, null);
+
+    _root!.Add(item, _comparer);
+    ++Count;
+  }
+
+  public void Clear()
+  {
+    _root = null;
+    Count = 0;
+  }
+
+  public bool Contains(TData item) => throw new NotImplementedException();
+
+  public void CopyTo(TData[] array, int arrayIndex)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Remove(TData item) => throw new NotImplementedException();
+
+  public IEnumerator<TData> GetEnumerator() => throw new NotImplementedException();
+
+  IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ src/DataStructures.Trees/BinarySearchTrees/Node.cs
@@ -0,0 +1,27 @@
+﻿namespace DataStructures.Trees.BinarySearchTrees;
+
+public record Node<TData>(TData Data, Node<TData>? Parent, Node<TData>? Left, Node<TData>? Right)
+{
+  public readonly TData Data = Data;
+  public Node<TData>? Parent { get; set; } = Parent;
+  public Node<TData>? Left { get; set; } = Left;
+  public Node<TData>? Right { get; set; } = Right;
+
+  public void Add(TData data, Comparer<TData> comparer)
+  {
+    if(comparer.Compare(Data, data) > 0)
+    {
+      if (Left is null)
+        Left = new Node<TData>(data, this, default, default);
+      else
+        Left.Add(data, comparer);
+    }
+    else
+    {
+      if (Right is null)
+        Right = new Node<TData>(data, this, default, default);
+      else
+        Right.Add(data, comparer);
+    }
+  }
+}
