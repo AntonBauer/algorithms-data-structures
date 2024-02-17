f4eb7032c55ce55e7ca990729ec6d2ecd250c17e
Anton Bauer <abton.bauer@gmail.com>
2024-02-17 21:30:58 +0100

    [Data structures, Binary Search Tree] add clear method tests

---
+++ test/DataStructures.Trees.Tests/BinarySearchTrees/ClearTests.cs
@@ -0,0 +1,35 @@
+﻿using DataStructures.Trees.BinarySearchTrees;
+
+namespace DataStructures.Trees.Tests.BinarySearchTrees;
+
+[TestFixture]
+public class ClearTests
+{
+  [Test(TestOf = typeof(BinarySearchTree<>))]
+  [Category("Data structures")]
+  [Category("Trees")]
+  public void Clear_EmptyTree_CountIsZero()
+  {
+    // Arrange
+    var tree = new BinarySearchTree<int>();
+
+    // Act
+    tree.Clear();
+
+    // Assert
+    Assert.That(tree, Is.Empty);
+  }
+  [Test(TestOf = typeof(BinarySearchTree<>))]
+  [Category("Data structures")]
+  [Category("Trees")]
+  public void Clear_NonEmptyTree_CountIsZero()
+  {
+    // Arrange
+    var tree = new BinarySearchTree<int> { 5, 3};
+
+    // Act
+    tree.Clear();
+
+    // Assert
+    Assert.That(tree, Is.Empty);
+  }}
