3f017f017f969810f2c83b73be91fab46a5c72d3
Anton Bauer <bauer_av@proton.me>
2024-02-25 20:21:03 +0100

    [Data structures, Doubly linked list] implement IList interface for doubly linked list

--- src/DataStructures.LinkedLists/DoublyLinkedLists/DoublyLinkedList.cs
+++ src/DataStructures.LinkedLists/DoublyLinkedLists/DoublyLinkedList.cs
@@ -1,60 +1,162 @@
-
 using System.Collections;
+
+namespace DataStructures.LinkedLists.DoublyLinkedLists;
 
 public class DoublyLinkedList<TData> : IList<TData>
 {
-    public TData this[int index] { get; set; }
+  private Node<TData>? _head;
+  private Node<TData>? _tail;
 
-    public int Count { get; }
-    public bool IsReadOnly { get; }
-
-    public void Add(TData item)
+  public TData this[int index]
+  {
+    get
     {
-        throw new NotImplementedException();
+      if (index < 0 || index >= Count)
+        throw new ArgumentOutOfRangeException(nameof(index), index, "Index is out of range");
+
+      var currentIndex = 0;
+      var currentItem = _head;
+      while (currentIndex < index)
+      {
+        currentItem = currentItem!.Next;
+        ++currentIndex;
+      }
+
+      return currentItem!.Data;
+    }
+    set => Insert(index, value);
+  }
+
+  public int Count { get; private set; }
+
+  public bool IsReadOnly => false;
+
+  public void Add(TData item)
+  {
+    if (Count == 0)
+      _head = _tail = new Node<TData>(item, null, null);
+    else
+    {
+      var node = new Node<TData>(item, _tail, null);
+      _tail!.Next = node;
+      _tail = node;
     }
 
-    public void Clear()
+    ++Count;
+  }
+
+  public void Clear()
+  {
+    _head = _tail = null;
+    Count = 0;
+  }
+
+  public bool Contains(TData item) => IndexOf(item) >= 0;
+
+  public void CopyTo(TData[] array, int arrayIndex)
+  {
+    if (arrayIndex + Count > array.Length)
+      throw new ArgumentOutOfRangeException(nameof(arrayIndex), arrayIndex, "Index is out of range");
+
+    foreach (var item in this)
+      array[arrayIndex++] = item;
+  }
+
+  public IEnumerator<TData> GetEnumerator()
+  {
+    var current = _head;
+    while (current is not null)
     {
-        throw new NotImplementedException();
+      yield return current.Data;
+      current = current.Next;
+    }
+  }
+
+  public int IndexOf(TData item)
+  {
+    if (Count == 0)
+      return -1;
+
+    var currentIndex = 0;
+    var currentItem = _head;
+    while (currentItem is not null)
+    {
+      if (currentItem.Data?.Equals(item) ?? false)
+        return currentIndex;
+
+      ++currentIndex;
+      currentItem = currentItem.Next;
     }
 
-    public bool Contains(TData item)
+    return -1;
+  }
+
+  public void Insert(int index, TData item)
+  {
+    if (index < 0 || index >= Count && Count != 0)
+      throw new ArgumentOutOfRangeException(nameof(index), index, "Index is out of range");
+
+    var current = _head;
+    var currentIndex = 0;
+
+    while (currentIndex + 1 < index)
     {
-        throw new NotImplementedException();
+      current = current!.Next;
+      ++currentIndex;
     }
 
-    public void CopyTo(TData[] array, int arrayIndex)
+    var newNode = new Node<TData>(item, current, current!.Next);
+    current.Next = newNode;
+    current.Next.Prev = newNode;
+
+    ++Count;
+  }
+
+  public bool Remove(TData item)
+  {
+    if (Count == 0)
+      return false;
+
+    var current = _head;
+    while (current is not null)
     {
-        throw new NotImplementedException();
+      if (current.Data?.Equals(item) ?? false)
+      {
+        Remove(current);
+        return true;
+      }
+
+      current = current.Next;
     }
 
-    public IEnumerator<TData> GetEnumerator()
+    return false;
+  }
+
+  public void RemoveAt(int index)
+  {
+    if (index < 0 || index >= Count)
+      throw new ArgumentOutOfRangeException(nameof(index), index, "Index is out of range");
+
+    var currentItem = _head;
+    var currentIndex = 0;
+    while (currentIndex < index)
     {
-        throw new NotImplementedException();
+      currentItem = _head!.Next;
+      ++currentIndex;
     }
 
-    public int IndexOf(TData item)
-    {
-        throw new NotImplementedException();
-    }
+    Remove(currentItem!);
+  }
 
-    public void Insert(int index, TData item)
-    {
-        throw new NotImplementedException();
-    }
+  IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-    public bool Remove(TData item)
-    {
-        throw new NotImplementedException();
-    }
+  private void Remove(Node<TData> node)
+  {
+    if (node.Prev is not null)
+      node.Prev.Next = node.Next;
+    if (node.Next is not null)
+      node.Next.Prev = node.Prev;
 
-    public void RemoveAt(int index)
-    {
-        throw new NotImplementedException();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
+    --Count;
+  }
 }
--- src/DataStructures.LinkedLists/DoublyLinkedLists/Node.cs
+++ src/DataStructures.LinkedLists/DoublyLinkedLists/Node.cs
@@ -1,10 +1,10 @@
+namespace DataStructures.LinkedLists.DoublyLinkedLists;
 
 internal sealed record Node<TData>(TData Data, Node<TData>? Prev, Node<TData>? Next)
 {
     public readonly TData Data = Data;
 
     public Node<TData>? Prev { get; set; } = Prev;
-
 
     public Node<TData>? Next { get; set; } = Next;
 }
