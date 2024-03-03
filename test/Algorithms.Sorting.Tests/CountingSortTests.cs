9c1f75c6e941b42de1a907b26baef6bcb46d7470
Anton Bauer <bauer_av@proton.me>
2024-03-03 15:52:09 +0100

    [Algorithms, Sorting] generalize counting sort algorighm

--- test/Algorithms.Sorting.Tests/CountingSortTests.cs
+++ test/Algorithms.Sorting.Tests/CountingSortTests.cs
@@ -9,12 +9,30 @@ public class CountingSortTests
   public void Should_sort_array_in_ascending_order()
   {
     // Arrange
-    int[] array = { 2, 5, 3, 0, 2, 3, 0, 3 };
+    var array = new int[] { 2, 5, 3, 0, 2, 3, 0, 3 };
 
     // Act
-    var result = array.CountingSort();
+    var result = array.CountingSort(i => i);
 
     // Assert
     Assert.That(result, Is.EqualTo(new int[] { 0, 0, 2, 2, 3, 3, 3, 5 }));
+  }
+
+
+  [Test(TestOf = typeof(CountingSortExtensions))]
+  [Category("Algorithms")]
+  [Category("Sorting")]
+  public void Should_sort_arbitrary_data_in_ascending_order()
+  {
+    // Arrange
+    var array = new int[] { 2, 5, 3, 0, 2, 3, 0, 3 };
+    var dict = array.Select(i => new KeyValuePair<int, string>(i, i.ToString())).ToArray();
+
+    // Act
+    var result = dict.ToArray().CountingSort(i => i.Key);
+
+    // Assert
+    var expected = new int[] { 0, 0, 2, 2, 3, 3, 3, 5 }.Select(i => new KeyValuePair<int, string>(i, i.ToString())).ToArray();
+    Assert.That(result, Is.EqualTo(expected));
   }
 }
