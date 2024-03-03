1b1870f76cde0d6c393e559f88cf4fb71104eb61
Anton Bauer <bauer_av@proton.me>
2024-03-03 21:53:16 +0100

    [Data structures, Suffix array] add Karkkainen-Sanders algorithm description to method body

--- src/DataStructures.SuffixArrays/SuffixArrayExtensions.cs
+++ src/DataStructures.SuffixArrays/SuffixArrayExtensions.cs
@@ -2,8 +2,24 @@
 
 public static class SuffixArrayExtensions
 {
+  /// <summary>
+  /// Implementation of Karkkainen-Sanders algorighm
+  /// </summary>
+  /// <param name="str">String to calculate suffix array for</param>
+  /// <returns>Suffix array</returns>
   public static int[] CalculateSuffixArray(this string str)
   {
+    // assign numbers to every char
+    // pad string to mod 3 == 0
+
+    // split into triplets
+    // take triplets starting at i mod 3 == 1 and i mod 3 == 2
+    // assign numbers by radix sort triplets
+    // if numbers are not distinct, recursively call suffix array on this new array
+    
+    // sort triplets starting at i mod 3 == 0
+
+    // Merge triplets arrays
 
     return Array.Empty<int>();
   }
--- test/DataStructures.SuffixArrays.Tests/SuffixArrayTests.cs
+++ test/DataStructures.SuffixArrays.Tests/SuffixArrayTests.cs
@@ -9,15 +9,16 @@ public class SuffixArrayTests
   [Test]
   [Category("Data structures")]
   [Category("Suffix arrays")]
-  public void Should_create_correct_suffix_array()
+  [TestCaseSource(nameof(TestCases))]
+  public void Should_create_correct_suffix_array(string str, int[] expectedArray)
   {
-    // Arrange
-    var str = "banana";
+    var actualArray = str.CalculateSuffixArray();
+    Assert.That(actualArray, Is.EqualTo(expectedArray));
+  }
 
-    // Act
-    var result = str.CalculateSuffixArray();
-
-    // Assert
-    Assert.That(result, Is.EqualTo(new int[] { 6, 4, 2, 1, 5, 3 }));
+  public static IEnumerable<TestCaseData> TestCases()
+  {
+    yield return new TestCaseData("banana", new int[] { 6, 4, 2, 1, 5, 3 }).SetName("Banana");
+    yield return new TestCaseData("mississippi", new int[] { 10, 7, 4, 1, 0, 9, 8, 6, 3, 5, 2 }).SetName("Mississippi");
   }
 }
