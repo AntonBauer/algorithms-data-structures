7e6e225c0c11228fed2e48a75f7cacaad662f3be
Anton Bauer <bauer_av@proton.me>
2024-03-05 21:29:02 +0100

    [Benchmarks] add counting sort benchmark

---
+++ benchmarks/Algorithms/CountingSortBenchmark.cs
@@ -0,0 +1,37 @@
+using Algorithms.Sorting;
+using BenchmarkDotNet.Attributes;
+
+namespace Algorithms;
+
+public class CountingSortBenchmark
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+  // Justification: is filled during setup
+  private int[] data;
+#pragma warning restore CS8618
+
+  [Params(10, 100, 10_000, 1_000_000)]
+  public int CollectionSize;
+
+  [GlobalSetup]
+  public void Setup()
+  {
+    data = new int[CollectionSize];
+    var random = new Random(15);
+
+    for (var i = 0; i < CollectionSize; i++)
+      data[i] = random.Next();
+  }
+
+  [Benchmark]
+  public int[] CountingSort() => data.ToArray().CountingSort(i => i);
+
+  [Benchmark]
+  public int[] BuiltInSort()
+  {
+    var toSort = data.ToArray();
+    Array.Sort(toSort);
+
+    return toSort;
+  }
+}
